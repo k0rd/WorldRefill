bc46e649ae548f3b7bbc51b5ab6e4cccee41edf6
k0rd <k0rd@gigganigga.net>
2012-02-18 01:23:17 -0500

    initial commit

---
+++ WorldRefill/WorldRefill.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Hooks;
+using TShockAPI;
+using Terraria;
+namespace WorldRefill
+{
+    [APIVersion(1, 11)]
+    public class WorldRefill : TerrariaPlugin
+    {
+        public WorldRefill(Main game)
+            : base(game)
+        {
+        }
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("causeevents", DoCrystals, "gencrystals")); //Life Crystals
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+            base.Dispose(disposing);
+        }
+
+        public override Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+        public override string Name
+        {
+            get { return "World Refill Plugin"; }
+        }
+        public override string Author
+        {
+            get { return "by k0rd"; }
+        }
+        public override string Description
+        {
+            get { return "Refill your world!"; }
+        }
+
+
+
+
+        private void DoCrystals(CommandArgs args)
+        {
+
+            if (args.Parameters.Count == 1)
+            {
+                var mCry = Int32.Parse(args.Parameters[0]);
+                var surface = Main.worldSurface;
+                var trycount = 0;
+                const int maxtries = 100000;
+                var realcount = 0;
+
+                while (trycount < maxtries)
+                {
+                    if (WorldGen.AddLifeCrystal(WorldGen.genRand.Next(1, Main.maxTilesX), WorldGen.genRand.Next((int)(surface + 20.0), Main.maxTilesY)))
+                    {
+                        realcount++;
+                        if (realcount == mCry) break;
+                    }
+                    trycount++;
+                }
+                args.Player.SendMessage(string.Format("Generated and hid {0} Life Crystals.",realcount));
+
+            }
+            else
+            {
+                args.Player.SendMessage(string.Format("Usage: /gencrystals (number of crystals to generate)"));
+            }
+            
+        }
+    }
+}
