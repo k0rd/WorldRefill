90754141a1c0ddb503a773d9161eae368170b45a
k0rd <k0rd@gigganigga.net>
2012-02-18 03:26:42 -0500

    added altars and shadow orbs

--- WorldRefill/WorldRefill.cs
+++ WorldRefill/WorldRefill.cs
@@ -18,7 +18,9 @@ namespace WorldRefill
         public override void Initialize()
         {
             Commands.ChatCommands.Add(new Command("causeevents", DoCrystals, "gencrystals")); //Life Crystals
-            Commands.ChatCommands.Add(new Command("causeevents", DoPots, "genpots")); //Pots
+            Commands.ChatCommands.Add(new Command("causeevents", DoPots, "genpots"));         //Pots
+            Commands.ChatCommands.Add(new Command("causeevents", DoOrbs, "genorbs"));         //Orbs
+            Commands.ChatCommands.Add(new Command("causeevents", DoAltars, "genaltars"));     //Demon Altars
         }
 
         protected override void Dispose(bool disposing)
@@ -46,10 +48,6 @@ namespace WorldRefill
         {
             get { return "Refill your world!"; }
         }
-
-
-
-
         private void DoCrystals(CommandArgs args)
         {
 
@@ -94,8 +92,6 @@ namespace WorldRefill
                 {
                     var tryX = WorldGen.genRand.Next(1, Main.maxTilesX);
                     var tryY = WorldGen.genRand.Next((int) surface - 10, Main.maxTilesY);
-
-
                         if (WorldGen.PlacePot(tryX,tryY, 28))
                         {
                             realcount++;
@@ -112,5 +108,74 @@ namespace WorldRefill
                 args.Player.SendMessage(string.Format("Usage: /genpots (number of pots to generate)"));
             }
         }
+
+        private void DoOrbs(CommandArgs args)
+        {
+            if (args.Parameters.Count == 1)
+            {
+
+                var mOrb = Int32.Parse(args.Parameters[0]);
+                var surface = Main.worldSurface;
+                var trycount = 0;
+                const int maxtries = 1000000;
+                var realcount = 0;
+                while (trycount < maxtries)
+                {
+                    var tryX = WorldGen.genRand.Next(1, Main.maxTilesX);
+                    var tryY = WorldGen.genRand.Next((int)surface + 20, Main.maxTilesY);
+
+                    if ((!Main.tile[tryX, tryY].active) && (Main.tile[tryX, tryY].wall == (byte)3))
+                    {
+                        WorldGen.AddShadowOrb(tryX, tryY);
+                        if (Main.tile[tryX, tryY].type == 31)
+                        {
+                            realcount++;
+                            if (realcount == mOrb)
+                                break;
+                        }
+                    }
+                    trycount++;
+                }
+                args.Player.SendMessage(string.Format("Generated and hid {0} Orbs.", realcount));
+            }
+            else
+            {
+                args.Player.SendMessage(string.Format("Usage: /genorbs (number of orbs to generate)"));
+            }
+        }
+        private void DoAltars(CommandArgs args)
+        {
+            if (args.Parameters.Count == 1)
+            {
+
+                var mAltar = Int32.Parse(args.Parameters[0]);
+                var surface = Main.worldSurface;
+                var trycount = 0;
+                const int maxtries = 1000000;
+                var realcount = 0;
+                while (trycount < maxtries)
+                {
+                    var tryX = WorldGen.genRand.Next(1, Main.maxTilesX);
+                    var tryY = WorldGen.genRand.Next((int)surface + 10, Main.maxTilesY);
+
+                        WorldGen.Place3x2(tryX, tryY, 26);
+                        if (Main.tile[tryX, tryY].type == 26)
+                        {
+                            realcount++;
+                            if (realcount == mAltar)
+                                break;
+                        }
+                    
+                    trycount++;
+                }
+                args.Player.SendMessage(string.Format("Generated and hid {0} Demon Altars.", realcount));
+            }
+            else
+            {
+                args.Player.SendMessage(string.Format("Usage: /genaltars (number of Demon Altars to generate)"));
+            }
+        }
+
+
     }
 }
