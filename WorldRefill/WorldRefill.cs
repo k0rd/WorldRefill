b24cb0ddd6d45de961298e4930a5c2b55bb010d2
k0rd <k0rd@gigganigga.net>
2012-02-19 21:12:48 -0500

    added webs

--- WorldRefill/WorldRefill.cs
+++ WorldRefill/WorldRefill.cs
@@ -23,6 +23,8 @@ namespace WorldRefill
             Commands.ChatCommands.Add(new Command("causeevents", DoAltars, "genaltars"));     //Demon Altars
             Commands.ChatCommands.Add(new Command("causeevents", DoTraps, "gentraps"));       //Traps
             Commands.ChatCommands.Add(new Command("causeevents", DoStatues, "genstatues"));   //Statues
+            Commands.ChatCommands.Add(new Command("causeevents", DoOres, "genores"));         //ores
+            Commands.ChatCommands.Add(new Command("causeevents", DoWebs, "genwebs"));         //webs
         }
 
         protected override void Dispose(bool disposing)
@@ -50,7 +52,7 @@ namespace WorldRefill
         {
             get { return "Refill your world!"; }
         }
-        public void InformPlayers()
+        public static void InformPlayers()
         {
             foreach (TSPlayer person in TShock.Players)
             {
@@ -200,7 +202,7 @@ namespace WorldRefill
                 var mTrap = Int32.Parse(args.Parameters[0]);
                 var surface = Main.worldSurface;
                 var trycount = 0;
-                const int maxtries = 4000;
+                const int maxtries = 100000;
                 var realcount = 0;
                 while (trycount < maxtries)
                 {
@@ -333,7 +335,7 @@ namespace WorldRefill
                 if (stid < 44)
                 {
 
-
+                    args.Player.SendMessage(string.Format("Generating {0} statues.. this may take a while..", found));
                     while (trycount < maxtries)
                     {
                         var tryX = WorldGen.genRand.Next(20, Main.maxTilesX - 20);
@@ -370,6 +372,235 @@ namespace WorldRefill
             else
             {
                 args.Player.SendMessage(string.Format("Usage: /genstatues (number of statues to generate) [(optional)name of statue]"));
+            }
+        }
+
+        private static void DoOres(CommandArgs args)
+        {
+            if (WorldGen.genRand == null)
+                WorldGen.genRand = new Random();
+
+            TSPlayer ply = args.Player;
+
+
+
+            int num = WorldGen.altarCount % 3;
+            int num2 = WorldGen.altarCount / 3 + 1;
+            float num3 = (float)(Main.maxTilesX / 4200);
+            int num4 = 1 - num;
+            num3 = num3 * 310f - (float)(85 * num);
+            num3 *= 0.85f;
+            num3 /= (float)num2;
+
+            if (args.Parameters.Count < 1)
+            {
+                ply.SendMessage("Usage: /genores (type) (amount)", Color.Red);    //should this be a help message instead?
+                return;
+            }
+            else if (args.Parameters[0] == "cobalt")
+            {
+                num = 0;
+            }
+            else if (args.Parameters[0] == "mythril")
+            {
+                num = 1;
+            }
+            else if (args.Parameters[0] == "copper")
+            {
+                num = 3;
+            }
+            else if (args.Parameters[0] == "iron")
+            {
+                num = 4;
+            }
+            else if (args.Parameters[0] == "silver")
+            {
+                num = 6;
+            }
+            else if (args.Parameters[0] == "gold")
+            {
+                num = 5;
+            }
+            else if (args.Parameters[0] == "demonite")
+            {
+                num = 7;
+            }
+            else if (args.Parameters[0] == "sapphire")
+            {
+                num = 8;
+            }
+            else if (args.Parameters[0] == "ruby")
+            {
+                num = 9;
+            }
+            else if (args.Parameters[0] == "emerald")
+            {
+                num = 10;
+            }
+            else if (args.Parameters[0] == "topaz")
+            {
+                num = 11;
+            }
+            else if (args.Parameters[0] == "amethyst")
+            {
+                num = 12;
+            }
+            else if (args.Parameters[0] == "diamond")
+            {
+                num = 13;
+            }
+            else
+            {
+                num = 2;
+            }
+
+            if (num == 0)
+            {
+                num = 107;
+                num3 *= 1.05f;
+            }
+            else if (num == 1)
+            {
+                num = 108;
+            }
+            else if (num == 3)
+            {
+                num = 7;
+                num3 *= 1.1f;
+            }
+            else if (num == 4)
+            {
+                num = 6;
+                num3 *= 1.1f;
+            }
+            else if (num == 5)
+            {
+                num = 8;
+                num3 *= 1.1f;
+            }
+            else if (num == 6)
+            {
+                num = 9;
+                num3 *= 1.1f;
+            }
+            else if (num == 7)
+            {
+                num = 22;
+                num3 *= 1;
+            }
+            else if (num == 8)
+            {
+                num = 63;
+                num3 *= .80f;
+            }
+            else if (num == 9)
+            {
+                num = 64;
+                num3 *= 1;
+            }
+            else if (num == 10)
+            {
+                num = 65;
+                num3 *= 1;
+            }
+            else if (num == 11)
+            {
+                num = 66;
+                num3 *= 1;
+            }
+            else if (num == 12)
+            {
+                num = 67;
+                num3 *= 1;
+            }
+            else if (num == 13)
+            {
+                num = 68;
+                num3 *= 1;
+            }
+            else
+            {
+                num = 111;
+            }
+
+
+            if (args.Parameters.Count > 1)
+            {
+                float.TryParse(args.Parameters[1], out num3);
+                num3 = Math.Min(num3, 10000f);
+            }
+
+            int num5 = 0;
+            while ((float)num5 < num3)
+            {
+                int i2 = WorldGen.genRand.Next(100, Main.maxTilesX - 100);
+                double num6 = Main.worldSurface;
+                if ((num == 108) || (num == 6) || (num == 7) || (num == 8) || (num == 9) || ((num > 62) && (num < 69)))
+                {
+                    num6 = Main.rockLayer;
+                }
+                if ((num == 111) || (num == 22) || (num == 68))
+                {
+                    num6 = (Main.rockLayer + Main.rockLayer + (double)Main.maxTilesY) / 3.0;
+                }
+                int j2 = WorldGen.genRand.Next((int)num6, Main.maxTilesY - 150);
+                WorldGen.OreRunner(i2, j2, (double)WorldGen.genRand.Next(5, 9 + num4), WorldGen.genRand.Next(5, 9 + num4), num);
+                num5++;
+            }
+            ply.SendMessage(String.Format("Spawned {0} tiles of {1}", Math.Floor(num3), num), Color.Green);
+            InformPlayers();
+        }
+        private void DoWebs(CommandArgs args)
+        {
+            if (args.Parameters.Count == 1)
+            {
+
+                var mWeb = Int32.Parse(args.Parameters[0]);
+                var surface = Main.worldSurface;
+                var trycount = 0;
+                const int maxtries = 1000000;
+                var realcount = 0;
+                while (trycount < maxtries)
+                {
+                    var tryX = WorldGen.genRand.Next(20, Main.maxTilesX-20);
+                    var tryY = WorldGen.genRand.Next(150, Main.maxTilesY-300);
+                    int direction = WorldGen.genRand.Next(2);
+                    if (direction == 0)
+                    {
+                        direction = -1;
+                    }
+                    else
+                    {
+                        direction = 1;
+                    }
+                     while (!Main.tile[tryX, tryY].active && tryY>149)
+                     {
+                         tryY--;
+                     }
+                    tryY++;
+                     while (!Main.tile[tryX, tryY].active && tryX > 10 && tryX < Main.maxTilesX - 10)
+                     {
+                         tryX += direction;
+                     }
+                    tryX -= direction;
+                    
+                    if ((tryY< Main.maxTilesY - 300 )&& (tryX <Main.maxTilesX - 20) && (tryX>20) && (tryY>150))
+                    {
+
+                         WorldGen.TileRunner(tryX, tryY, (double)WorldGen.genRand.Next(4, 11), WorldGen.genRand.Next(2, 4), 51, true, (float)direction, -1f, false, false);
+                        realcount++;
+                        if (realcount == mWeb)
+                            break;
+                    }
+                    trycount++;
+
+                }
+                args.Player.SendMessage(string.Format("Generated and hid {0} Webs.", realcount));
+                InformPlayers();
+            }
+            else
+            {
+                args.Player.SendMessage(string.Format("Usage: /genwebs (number of webs to generate)"));
             }
         }
 
