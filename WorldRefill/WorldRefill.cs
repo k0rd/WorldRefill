9e2420ae2be58e4acfcb0c5b870068aa1f0ac843
k0rd <k0rd@gigganigga.net>
2012-02-19 21:24:48 -0500

    removed some unused assembly references

--- WorldRefill/WorldRefill.cs
+++ WorldRefill/WorldRefill.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using Hooks;
 using TShockAPI;
 using Terraria;
 namespace WorldRefill
